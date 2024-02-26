70931edfc3ee537bc6ce1054e9ee2902fbdc4f54
Sun Jiayu <1049867781@qq.com>
2024-02-26 20:46:59 +0800

    0226修改

--- 前端初始文件/sjy/管理信息/index.js
+++ 前端初始文件/sjy/管理信息/index.js
@@ -1,101 +1,123 @@
-// // 获取容器元素
-// let container = document.querySelector('.studentList');
-
-// // 将新的 div 元素追加到容器元素中
-// container.appendChild(div2);
-
-//创建数组，目标是根据数组数据个数动态生成按钮
-let objArray = [
+//创建数组，目标是根据数组数据个数动态生成按钮，将数组的值赋给响应标签
+var objArray = [
     { name: "李四", number: "123456", direction: "全栈" },
     { name: "张三", number: "234567", direction: "全栈" },
+    { name: "张三", number: "232567", direction: "全栈" },
+    { name: "张三", number: "237567", direction: "全栈" },
+    { name: "张三", number: "238567", direction: "全栈" },
+    { name: "张三", number: "231567", direction: "全栈" },
+    { name: "张三", number: "2387567", direction: "全栈" },
+    { name: "张三", number: "234597", direction: "全栈" },
 ];
 
-// 每页显示的项目数量  
-let itemsPerPage = 6;
+//设置每页的元素个数，perPage可以用var设置为全局变量
+//注意，当前页面currentPage不可以设置为全局变量，因为它会不断变化  
+//应该在函数内用 let 声明 currentPage
+var perPage = 6;
 
-// 当前页码  
-let currentPage = 1;
+//计算最大页码
+var totalPages = Math.ceil(objArray.length / perPage);
 
-// 渲染列表  
-function renderList(items, start, end) {
+var list = document.querySelector('.studentList');
+//清空class = studentList 标签内的内容
+list.innerHTML = ' ';
+    
+console.log(list.innerHTML);
 
-//先清空列表  
-    let list = document.querySelector('.studentList');
-    list.innerHTML = ''; 
+// 渲染页面中的列表  
+function renderList(currentPage) {
+    let startIndex = (currentPage - 1) * perPage;
+    let endIndex = startIndex + perPage;
+    if (endIndex > objArray.length) {
+        endIndex = objArray.length;
+    }
 
-//for循环遍历添加元素，start为0，end为每页显示的项目数量itemsPerPage,满足小于6且小于数组长度时进行
-    for (let i = start; i < end && i < items.length; i++) {
-
-        //此处将创建的新元素绑定为studentMessage
-        //  let container = document.createElement('');
-        // 分别创建元素
-        let div1 = document.createElement('div');
-        div1.className = "studentList";
-
-        let div2 = document.createElement('div');
-        div2.className = "studentMessage";
-
+    //先跳过console
+    
+    for (let i = startIndex; i < endIndex; i++) {
+        //创建一个标签模板，用来显示数组中的数字
+        
+        let divStudentMessage = document.createElement('div');
+        divStudentMessage.className = "studentMessage";
+        
         let img = document.createElement('img');
+        img.src = `ai-image@3x.png`;
         img.className = "portrait";
 
-        let span1= document.createElement('span');
-        span1.classaName = "message";
+        let spanMessage= document.createElement('span');
+        spanMessage.className = "message";
 
-        let div3 = document.createElement('div');
-        div3.className = "name";
+        let divName = document.createElement('div');
+        divName.className = "name";
 
-        let div4 = document.createElement('div');
-        div4.className = "number";
+        let divNumber = document.createElement('div');
+        divNumber.className = "number";
 
-        let div5 = document.createElement('div');
-        div5.className = "direction";
+        let divDirection = document.createElement('div');
+        divDirection.className = "direction";
+
+        divName.innerHTML = objArray[i].name;
+        divNumber.innerHTML = objArray[i].number;
+        divDirection.innerHTML = objArray[i].direction;
+        
         //利用appendChild向span1中添加子节点，一次只能添加一个
-        span1.appendChild(div3);
-        span1.appendChild(div4);
-        span1.appendChild(div5);
-        div3.textContent = items[i].name;
-        div4.textContent = items[i].number;
-        div5.textContent = items[i].direction;
-        let span2= document.createElement('span');
-        span2.className = "button";
-        let span3= document.createElement('span');
-        span3.className = "buttonReset";
-        let span4= document.createElement('span');
-        span4.className = "buttonAlter";
-        let span5= document.createElement('span');
-        span5.className = "buttonDelete";
+        spanMessage.appendChild(divName);
+        spanMessage.appendChild(divNumber);
+        spanMessage.appendChild(divDirection);
+        
+        let spanButton= document.createElement('span');
+        spanButton.className = "button";
+        let spanReset= document.createElement('span');
+        spanReset.className = "buttonReset";
+        let spanAlter= document.createElement('span');
+        spanAlter.className = "buttonAlter";
+        let spanDelete= document.createElement('span');
+        spanDelete.className = "buttonDelete";
 
         //利用appendChild向span2中添加子节点，一次只能添加一个
-        span2.appendChild(span3);
-        span2.appendChild(span4);
-        span2.appendChild(span5);
+        spanButton.appendChild(spanReset);
+        spanButton.appendChild(spanAlter);
+        spanButton.appendChild(spanDelete);
 
         //利用appendChild向div2中添加子节点,div2就是集合所有内容的节点
-        div2.appendChild(img);
-        div2.appendChild(span1);
-        div2.appendChild(span2);
-        
-        list.appendChild(div2);
+        divStudentMessage.appendChild(img);
+        divStudentMessage.appendChild(spanMessage);
+        divStudentMessage.appendChild(spanButton);
+            
+        //以上，节点模板书写完成
+
+        list.appendChild(divStudentMessage);
     }
 }
-
 // 渲染分页按钮  
-function renderPagination(totalPages) {
+function renderPagination() {
+    // 清空分页控件  
     var pagination = document.querySelector('.pagination');
-    pagination.innerHTML = ''; // 清空分页控件  
+    pagination.innerHTML = '';
+
+    //声明当前页面
+    let currentPage = 1;
 
     for (var i = 1; i <= totalPages; i++) {
-        var button = document.createElement('button');
-        button.textContent = i;
-        button.addEventListener('click', function () {
-            currentPage = parseInt(this.textContent);
-            renderList(objArray, (currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-        });
+        button = document.createElement('button'); button.innerHTML = i;
         pagination.appendChild(button);
+        
+        // 必须为每个按钮添加**独立**的事件监听器
+        button.addEventListener('click', function () {
+            list.innerHTML = '';
+
+            // console.log(typeof button.innerHTML) ==> string
+            //利用parseInt将button上的内容由字符串转换为整型数字，赋值给当前页面
+
+            renderList(parseInt(this.innerHTML));
+        });
     }
+  
 }
+// //先用一个renderList显示第一页列表，再切换
+// renderList(1);
+// //调用分页函数，控制页面数
+// renderPagination();
 
-// 初始渲染  
-renderList(objArray, 0, itemsPerPage);
-renderPagination(Math.ceil(objArray.length / itemsPerPage));
 
+//检查LIst函数中current处逻辑
